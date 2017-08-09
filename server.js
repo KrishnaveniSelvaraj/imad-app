e4672fa219644f85011153905ddd9844412ce2ef
KrishnaveniSelvaraj <30790934+KrishnaveniSelvaraj@users.noreply.github.com>
2017-08-09 13:01:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,10 +52,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+//counter
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+})
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+//url
 app.get('/article-one', function (req, res) {
   res.send(CreateTemplate(articleOne));
 });
