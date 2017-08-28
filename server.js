48bbdb7177c948477a5cf755c8567b1959bec93d
KrishnaveniSelvaraj <30790934+KrishnaveniSelvaraj@users.noreply.github.com>
2017-08-28 11:51:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
+
 
 var articleOne={
     title:'ArticleOne',
@@ -51,6 +54,7 @@ function CreateTemplate(data)
 }
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -74,7 +78,7 @@ app.post('/create-user',function(req,res)
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var dbString=hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result) {
         if(err){
             res.status(500).send(err.toString());
         }else
