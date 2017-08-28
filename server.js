a8ee75a4b38280833da1c9c26f018711bd3086e2
KrishnaveniSelvaraj <30790934+KrishnaveniSelvaraj@users.noreply.github.com>
2017-08-28 11:13:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ function hash(input,salt){
 
 app.get('/hash/:input',function(req,res){
     var hashedstring=hash(req.params.input,'this-is-some-random-string');
-    res.send(hashedString);
+    res.send(hashedstring);
     
 });
 
