28d011d8ecbd965feb83ecd1546d7ae7bcc1976e
KrishnaveniSelvaraj <30790934+KrishnaveniSelvaraj@users.noreply.github.com>
2017-08-28 11:13:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
-    return hashed.tostring('hex');
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input',function(req,res){
